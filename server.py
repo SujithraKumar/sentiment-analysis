e8f45feda645d83462a61b71b006468b6bfb5a72
sujithra <sujithrakumar13@gmail.com>
2018-05-08 15:18:33 +0530

    senti code

---
+++ server.py
@@ -0,0 +1,88 @@
+from flask import Flask, render_template, request, jsonify, make_response
+from info import classify2
+from math import e
+from redis import Redis
+
+from cors import crossdomain
+from datetime import datetime
+import json
+
+apicount = 0
+app = Flask(__name__)
+app.debug = False
+app.config['MAX_CONTENT_LENGTH'] = (1 << 20) # 1 MB max request size
+#conn = Redis(RHOST, RPORT, password=RPASS)
+
+
+def percentage_confidence(conf):
+	return 100.0 * e ** conf / (1 + e**conf)
+
+def today():
+	return datetime.now().strftime('%Y-%m-%d')
+
+def get_sentiment_info(text):
+	flag, confidence = classify2(text)
+	if confidence > 0.5:
+		sentiment = "Positive" if flag else "Negative"
+	else:
+		sentiment = "Neutral"
+	conf = "%.4f" % percentage_confidence(confidence)
+	return (sentiment, conf)
+
+@app.route('/')
+def home():
+	#conn.incr(STATS_KEY + "_hits")
+	return render_template("index.html")
+
+@app.route('/api/text/', methods=["POST"])
+@crossdomain(origin='*')
+def read_api():
+	text = request.form.get("txt")
+	sentiment, confidence = get_sentiment_info(text)
+	result = {"sentiment": sentiment, "confidence": confidence}
+        global apicount
+        apicount+=1
+	#conn.incr(STATS_KEY + "_api_calls")
+	#conn.incr(STATS_KEY + today())
+	return jsonify(result=result)
+
+@app.route('/web/text/', methods=["POST"])
+@crossdomain(origin='*')
+def evaldata():
+	text = request.form.get("txt")
+	result, confidence = get_sentiment_info(text)
+        global apicount
+        apicount+=1
+	#conn.incr(STATS_KEY + "_web_calls")
+	#conn.incr(STATS_KEY + today())
+	return jsonify(result=result, confidence=confidence, sentence=text)
+
+@app.route('/api/batch/', methods=["POST"])
+@crossdomain(origin='*')
+def batch_handler():
+	json_data = request.get_json(force=False, silent=True)
+        print "The JSON data is"
+        print json_data   
+        #return jsonify(success=json_data)
+	if not json_data:
+		return jsonify(error="Bad JSON request")
+   	result = []
+        global apicount 
+        apicount+=len(json_data)
+	for req in json_data:
+		sent, conf = get_sentiment_info(req)
+		result.append({"result": sent, "confidence": conf, "value": req})
+        #apicount+=1
+        print "api count"
+        print apicount
+        
+
+	#conn.incrby(STATS_KEY + "_api_calls", len(json_data))
+	#conn.incrby(STATS_KEY + today(), len(json_data))
+	resp = make_response(json.dumps(result))
+	resp.mimetype = 'application/json'
+
+	return resp
+
+
+
---
+++ static/app.js
@@ -0,0 +1,9 @@
+var btn = document.getElementById('submit');
+var tpl = _.template(document.getElementById('templ-results').innerHTML);
+btn.addEventListener("click", function(event) {
+	var txt = document.getElementById('txt').value;
+	$.post('web/text/', {"txt": txt}, function (data) {		
+		document.getElementById('result').innerHTML = tpl(data);
+		//console.log(tpl(data))
+	});
+});
